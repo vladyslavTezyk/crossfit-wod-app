9ee5afa04f6022acbb182c0c966ba802f3123010
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-01-12 15:57:02 +0100

    feat: add basic workoutService

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+import workoutService from "../services/workoutService"
+
 function getAllWorkouts(req,res){
+    const allWorkouts = workoutService.getAllWorkouts()
     res.send('Get all workouts')
 }
 
 function getOneWorkout(req, res){
+    const workout = workoutService.getOneWorkout()
     res.send(`Get an existing workout ${req.params.workoutId}`)
 }
 
 function createNewWorkout(req, res){
+    const createdWorkout = workoutService.createNewWorkout()
     res.send('Create a new workout')
 }
 
 function updateOneWorkout(req, res){
+    const updatedWorkout = workoutService.updateOneWorkout()
     res.send('Update an existing workout')
 }
 
 function deleteOneWorkout(req, res){
+    const deletedWorkout = workoutService.deleteOneWorkout()
     res.send('Delete an existing workout')
 }
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,27 @@
+function getAllWorkouts(){
+    return;
+}
+
+function getOneWorkout(){
+    return;
+}
+
+function createNewWorkout(){
+    return;
+}
+
+function updateOneWorkout(){
+    return;
+}
+
+function deleteOneWorkout(){
+    return;
+}
+
+export default {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout
+}
